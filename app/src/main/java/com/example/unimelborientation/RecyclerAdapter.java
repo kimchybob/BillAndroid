c0448f96d425e1133e33ec6e5fbd92bc934a0f2b
Ro8er <776030167@qq.com>
2021-10-05 16:07:01 +0800

    [Enhance] recycle view: hold faked list data [New] define row subject class

--- app/src/main/java/com/example/unimelborientation/RecyclerAdapter.java
+++ app/src/main/java/com/example/unimelborientation/RecyclerAdapter.java
@@ -10,14 +10,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.unimelborientation.type.RowSubject;
+
+import java.util.List;
+
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
     private static final String TAG = "RecyclerAdapter";
-    int count = 0;
+
+    List<RowSubject> subjectsList;
+
+    public RecyclerAdapter(List<RowSubject> subjectsList) {
+        this.subjectsList = subjectsList;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Log.i(TAG, "onCreateViewHolder: " + count++);
 
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.row_item, parent, false);
@@ -27,12 +35,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.rowSmallTextView.setText(subjectsList.get(position).getRowSmallText());
+        holder.rowLargeTextView.setText(subjectsList.get(position).getRowLargeText());
+        //Todo set subject score???
+        //Todo set subject title???
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+        return subjectsList.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/com/example/unimelborientation/SearchActivity.java
+++ app/src/main/java/com/example/unimelborientation/SearchActivity.java
@@ -1,26 +1,51 @@
 package com.example.unimelborientation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.unimelborientation.type.RowSubject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     RecyclerAdapter recyclerAdapter;
 
+    List<RowSubject> subjectsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        subjectsList = new ArrayList<>();
+
+
         recyclerView = findViewById(R.id.recyclerView);
-        recyclerAdapter = new RecyclerAdapter();
+        recyclerAdapter = new RecyclerAdapter(subjectsList);
 
 //        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         recyclerView.setAdapter(recyclerAdapter);
+
+        //faked data for test
+        for(int i=0; i<30; i++){
+            subjectsList.add(new RowSubject(
+                    "Subject" + i,
+                    "Comp9000" + i,
+                    0,
+                    0,
+                    0
+                    )
+            );
+        }
+        //Todo get real data from our database???
+
     }
 }
