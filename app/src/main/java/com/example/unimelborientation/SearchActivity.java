22e60482d03e80571a4c93b53467db69b1aba44f
Ro8er <776030167@qq.com>
2021-10-05 17:12:47 +0800

    [New] search function main_menu

--- app/src/main/java/com/example/unimelborientation/SearchActivity.java
+++ app/src/main/java/com/example/unimelborientation/SearchActivity.java
@@ -1,11 +1,14 @@
 package com.example.unimelborientation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.unimelborientation.type.RowSubject;
 
@@ -50,5 +53,25 @@ public class SearchActivity extends AppCompatActivity {
         }
         //Todo get real data from our database???
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
