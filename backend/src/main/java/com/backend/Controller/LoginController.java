5cf04b92a3a21c96a99f7394339e7bc7278bcb84
Tianyi Zheng <zhetz@student.unimelb.edu.au>
2021-11-02 13:42:17 +0800

    updating signup interface

--- backend/src/main/java/com/backend/Controller/LoginController.java
+++ backend/src/main/java/com/backend/Controller/LoginController.java
@@ -5,7 +5,11 @@ import com.backend.Domain.User;
 import com.backend.Service.LoginService;
 import com.backend.Util.AjaxResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class LoginController {
@@ -20,12 +24,22 @@ public class LoginController {
 //    }
 
     @PostMapping("/api/signup")
-    public AjaxResult insertUser(@RequestBody User user){
-        if(user==null||user.getUsername()==null||user.getPassword()==null)
+    public AjaxResult insertUser(@RequestParam("username") String username,
+                                 @RequestParam("password") String password,
+                                 @RequestParam("email") String email){
+        if(username==null||password==null||email==null)
             return AjaxResult.error("Input Missing!");
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setLasttime(new Date());
         int userId=loginService.insert(user);
-        if (userId == 0)
-            return AjaxResult.error("Signup fail!");
+        if (userId == 77777)
+            return AjaxResult.error("Username already exists!");
+        if (userId == 88888)
+            return AjaxResult.error("Email already exists!");
+
         return AjaxResult.success("Successfully signup!");
     }
 
