a9c8d8a52f480506dbfe8fe2783c35b3923df0ed
Ro8er <776030167@qq.com>
2021-11-04 19:34:37 +0800

    [New]log out function

--- app/src/main/java/com/example/unimelborientation/MainActivity.java
+++ app/src/main/java/com/example/unimelborientation/MainActivity.java
@@ -9,18 +9,23 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.unimelborientation.databinding.ActivityMainBinding;
+import com.example.unimelborientation.util.SharedPreferencesUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     ActivityMainBinding binding;
+    private SharedPreferencesUtils local_setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        local_setting = new SharedPreferencesUtils(this, "setting");
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 //bottom nav
@@ -32,5 +37,20 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, configuration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
+        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                local_setting.putValues(
+                        new SharedPreferencesUtils.ContentValue("rememberPassword", false),
+                        new SharedPreferencesUtils.ContentValue("autoLogin", false));
+
+                startActivity(new Intent(view.getContext(), UniLoginActivity.class));
+            }
+        }
+
+
+        );
+
     }
+
 }
